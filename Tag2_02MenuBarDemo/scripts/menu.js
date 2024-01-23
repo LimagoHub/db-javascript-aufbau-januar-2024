62a87bc3b4991adbbc390ab2522a48f92fbf313f
Jo Wagner <jowagner@limago.de>
2024-01-23 12:17:58 +0100

    uebung menu

---
+++ Tag2_02MenuBarDemo/scripts/menu.js
@@ -0,0 +1,28 @@
+class MenuBar {
+    
+    constructor(id) {
+
+    }
+
+    addMenu(menu) {
+
+    }
+
+}
+
+class Menu {
+    
+    constructor(label) {
+        
+    }
+
+    addMenuItem(menuItem) {
+
+    }
+}
+
+class MenuItem{
+    constructor(label, callback) {
+        
+    }
+}
